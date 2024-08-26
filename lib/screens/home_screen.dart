861102532d9d40fcdada3cfee52db6c477ad4934
YUapisit <88958116+Apisit250aps@users.noreply.github.com>
2024-08-26 20:50:32 +0700

    signin with token

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_form/screens/signup_screen.dart';
+import 'package:flutter_form/screens/signin_screen.dart';
+import 'package:get/get.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -11,26 +15,48 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   Future<void> _checkToken() async {
-    final prefs = await SharedPreferences.getInstance();
-    final token = prefs.getString('auth_token');
-
-    if (token == null || token.isEmpty) {
-      // ไม่มี token ให้ไปหน้า Login
-     
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => SignupScreen()),
-      );
-    } else {
-      // ถ้ามี token ก็อยู่ในหน้าหลักต่อไป
-      
+    final token = _readFromFile();
+    if (token == '' || token == null) {
+      Get.to(SigninScreen());
     }
   }
+
+  String _fileContent = "";
 
   @override
   void initState() {
     super.initState();
+
     _checkToken();
+  }
+
+  Future<String> _getFilePath() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return '${directory.path}/token.txt';
+  }
+
+  Future<void> _writeToFile(String content) async {
+    final filePath = await _getFilePath();
+    final file = File(filePath);
+    await file.writeAsString(content);
+    setState(() {
+      _fileContent = content;
+    });
+  }
+
+  Future<void> _readFromFile() async {
+    try {
+      final filePath = await _getFilePath();
+      final file = File(filePath);
+      final content = await file.readAsString();
+      setState(() {
+        _fileContent = content;
+      });
+    } catch (e) {
+      setState(() {
+        _fileContent = "Error reading file: $e";
+      });
+    }
   }
 
   @override
--- lib/widgets/forms/signin_form.dart
+++ lib/widgets/forms/signin_form.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
 
 class SigninForm extends StatefulWidget {
   const SigninForm({super.key});
@@ -11,12 +15,87 @@ class _SigninFormState extends State<SigninForm> {
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   bool _obscureText = true;
+  String _fileContent = "";
+
+  @override
+  void initState() {
+    super.initState();
+    _readFromFile();
+  }
+
+  Future<void> login() async {
+    final url = Uri.parse(
+        'https://wallet-api-7m1z.onrender.com/auth/login'); // แทนที่ด้วย URL ของ API จริง
+
+    try {
+      final response = await http.post(
+        url,
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(<String, String>{
+          'username': _usernameController.text,
+          'password': _passwordController.text,
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        // ถ้าการล็อกอินสำเร็จ (เช่นได้รับ HTTP 200)
+        final data = jsonDecode(response.body);
+        final token = data['token'] as String?; // การแคสต์เป็น String?
+        if (token != null) {
+          print('Login successful. Token: $token');
+          await _writeToFile(token);
+        } else {
+          print('Token not received');
+        }
+      } else {
+        // ถ้าการล็อกอินล้มเหลว (เช่นได้รับ HTTP 401)
+        print('Login failed with status: ${response.statusCode}');
+        print('Response: ${response.body}');
+      }
+    } catch (e) {
+      print('Error occurred: $e');
+    }
+  }
+
+  Future<String> _getFilePath() async {
+    final directory = await getApplicationDocumentsDirectory();
+    print('${directory.path}/token.txt');
+    return '${directory.path}/token.txt';
+  }
+
+  Future<void> _writeToFile(String content) async {
+    final filePath = await _getFilePath();
+    final file = File(filePath);
+    await file.writeAsString(content);
+    setState(() {
+      _fileContent = content;
+    });
+  }
+
+  Future<void> _readFromFile() async {
+    try {
+      final filePath = await _getFilePath();
+      final file = File(filePath);
+      final content = await file.readAsString();
+      setState(() {
+        _fileContent = content;
+      });
+    } catch (e) {
+      setState(() {
+        _fileContent = "Error reading file: $e";
+      });
+    }
+  }
+
   @override
   void dispose() {
     _usernameController.dispose();
     _passwordController.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -80,7 +159,7 @@ class _SigninFormState extends State<SigninForm> {
         ),
         const SizedBox(height: 25.0),
         InkWell(
-          onTap: null,
+          onTap: () => login(),
           child: Container(
             width: double.infinity,
             padding: const EdgeInsets.all(25),
